b47bdb6052f3b4322eacc06678832bb1fd57422d
N Skutt <nskutt@protonmail.com>
2023-02-24 16:55:10 -0600

    checks to see if the guess is correct, stores players guesses, checks for victory

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -10,6 +10,7 @@ class Game
     dictionary = File.read('google-10000-english-no-swears.txt')
     wordlist = dictionary.split.delete_if { |word| word.length < 5 || word.length > 12 }
     @secret_word = choose_word(wordlist)
+    @player_input = Array.new(@secret_word.length, '_')
   end
 
   def choose_word(list)
@@ -17,8 +18,24 @@ class Game
   end
 
   def check_word(guess)
-    p "#{@secret_word} : #{guess}"
-    p @secret_word.include?(guess)
+    if @secret_word.include?(guess)
+      indices = []
+      @secret_word.each_index { |idx| indices.push[idx] if @secret_word == guess }
+      storage(guess, indices)
+    else
+      p 'You guessed wrong!'
+  end
+
+  def storage(guess, locations)
+    location.each { |idx| @player_input[idx] = guess }
+    win_or_lose
+  end
+
+  def win_or_lose
+    if @player_input == @secret_word
+      p 'You win!'
+      endgame('win')
+    end
   end
 end
 
@@ -51,7 +68,7 @@ class Player
   end
 
   def error(code)
-    puts "\nWhat is your #{code}?"
+    puts "\nWhat is the #{code}?"
     instance_variable_set("@#{code}", gets.chomp.downcase)
     validate_input(@guess)
   end
